ac4d50ac1dcce65fc4e00486abc64daeb9dc2bcf
martin1952patrick <martinpati1952@gmail.com>
2023-10-28 06:52:46 +0300

    updated landing page

--- src/App.tsx
+++ src/App.tsx
@@ -62,6 +62,7 @@ function App() {
         <Stack minW="400px" marginTop="2rem" p="2rem" h="fit-content" gap={8}>
           <Heading fontSize="5xl" textAlign="center">
             Todo App
+            add your activities
           </Heading>
           <Flex gap={3}>
             <Input
